b3c22d4db30a4c7400c80f76de4e110922c420d0
amagicmuffin <62270558+amagicmuffin@users.noreply.github.com>
2021-10-06 10:36:05 -0700

    starting main game loop and Scanner

--- src/Cavernous.java
+++ src/Cavernous.java
@@ -1,23 +1,47 @@
 public class Cavernous {
+    private Map screen1;
+    private Player player1;
+    
+    public Cavernous() {
+        Map screen1 =  {{'#','#','#','#','#','#','#','#','#','#'},
+                        {'#','.','.','.','.','.','.','.','.','#'},
+                        {'#','.','.','.','.','.','.','.','.','#'},
+                        {'#','.','.','.','.','.','.','.','.','#'},
+                        {'#','#','#','#','#','#','#','#','#','#'}};
+        player1 = new Player('@',1,1);
+    }
+    
     public static void main(String[] args) {
-        char[][] screen1map =  {{'#','#','#','#','#','#','#','#','#','#'},
-                                {'#','.','.','.','.','.','.','.','.','#'},
-                                {'#','.','.','.','.','.','.','.','.','#'},
-                                {'#','.','.','.','.','.','.','.','.','#'},
-                                {'#','#','#','#','#','#','#','#','#','#'}};
-
-        Map screen1 = new Map(screen1map);
-
-        Player player1 = new Player('@', 1,1);
-
+        LoadCavernous();
+        
+        Scanner userInput = new Scanner(System.in); // TODO: does this need to be somewhere else?
+        
+        // main game loop
+        boolean gaming = true;
+        while (gaming) {
+            mapInput(getInput(userInput));
+        }
+    }
+    
+    /** Sets up screen1, renders screen **/
+    private void LoadCavernous() {
+        // loads screen (all that needs to be loaded rn is the player)
+        // TODO: may need a method to load screen?
+        screen1.setTile(player1.getiPos,player1.getjPos,player1.getPlayer);
+        
+        // renders screen
         screen1.render();
-        screen1.setTile(player1.getiPos(), player1.getjPos(), player1.getPlayer());
-        screen1.render();
-        player1.TryMove("d", screen1);
-        screen1.render();
-        player1.TryMove("d", screen1);
-        screen1.render();
+    }
+    
+    private String getInput(Scanner userInput) {
+        return userInput.nextLine();
+    }
+    
+    /** process input while viewing map **/
+    private void mapInput(String userInput) {
+        // switch statement to call functions if x,y,etc inputs are inputted
     }
 }
 
 // TODO: get started on main game loop, Scanner input
+// TODO: player class may need Player.currentScreen attribute
